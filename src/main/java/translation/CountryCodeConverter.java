8b7432d283f658ac660f811363edfa2a69aaf569
YCJiVanessa <yuchen.ji@mail.utoronto.ca>
2025-09-22 14:22:57 -0400

    task b ji

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -41,7 +41,12 @@ public class CountryCodeConverter {
             while (iterator.hasNext()) {
                 String line = iterator.next();
                 String[] parts = line.split("\t");
-                // TODO Task B: use parts to populate the instance variables
+                String countryName = parts[0].trim();
+                String countryCode = parts[2].trim().toUpperCase();
+
+                countryCodeToCountry.put(countryCode, countryName);
+                countryToCountryCode.put(countryName, countryCode);
+
             }
         }
         catch (IOException | URISyntaxException ex) {
@@ -56,7 +61,7 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
+        code = countryCodeToCountry.get(code.trim().toUpperCase());
         return code;
     }
 
@@ -66,7 +71,7 @@ public class CountryCodeConverter {
      * @return the 3-letter code of the country
      */
     public String fromCountry(String country) {
-        // TODO Task B: update this code to use an instance variable to return the correct value
+        country = countryToCountryCode.get(country);
         return country;
     }
 
@@ -75,7 +80,9 @@ public class CountryCodeConverter {
      * @return how many countries are included in this country code converter.
      */
     public int getNumCountries() {
-        // TODO Task B: update this code to use an instance variable to return the correct value
+        if  (!countryCodeToCountry.isEmpty()) {
+            return countryCodeToCountry.size();
+        }
         return 0;
     }
 }
